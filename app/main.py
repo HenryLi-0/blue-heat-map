7bb11d2298328c553633cf061d11418d70642fc4
HenryLi-0 <bxsci8266@gmail.com>
2025-02-09 22:56:40 -0500

    add LOTS of data storage optimization plans, and some program settings

--- app/main.py
+++ app/main.py
@@ -1,14 +1,32 @@
 from flask import Flask, render_template
+from threading import Thread
 
-app = Flask(__name__)
 
-@app.route('/')
-def test():
-    return render_template("test2.html")
+BACKEND = True
+FRONTEND = True
 
-@app.route('/original')
-def hmm():
-    return render_template("test.html")
 
-if __name__ == '__main__':
+'''MINECRAFT PROCESSING STUFF'''
+if BACKEND:
+    def listener():
+        while True:
+            pass
+
+    thread = Thread(target=listener)
+    thread.start()
+
+
+'''FLASK STUFF'''
+
+if FRONTEND:
+    app = Flask(__name__)
+
+    @app.route('/')
+    def test():
+        return render_template("test2.html")
+
+    @app.route('/original')
+    def hmm():
+        return render_template("test.html")
+
     app.run(debug=True, host="0.0.0.0", port=5000)
